18005334dfdc80eb3ae334fafd320ed708a70052
Jack Arthur Harrhy <me@jackharrhy.com>
2022-03-23 23:57:38 -0230

    march 23rd update

--- diagram.py
+++ diagram.py
@@ -1,6 +1,14 @@
 from diagrams import Diagram, Cluster
 from diagrams.onprem.compute import Server
-from diagrams.programming.language import Python, Java, Php, Rust, JavaScript, Go, Elixir
+from diagrams.programming.language import (
+    Python,
+    Java,
+    Php,
+    Rust,
+    JavaScript,
+    Go,
+    Elixir,
+)
 from diagrams.generic.storage import Storage
 from diagrams.generic.blank import Blank
 from diagrams.onprem.database import PostgreSQL
@@ -8,15 +16,45 @@ from diagrams.onprem.container import Docker
 from diagrams.onprem.network import Nginx
 from diagrams.custom import Custom
 
+
 class WebSite(Custom):
     def __init__(self, name, *args, **kwargs):
         super().__init__(name, "./images/web.png", *args, **kwargs)
+
 
 class Crystal(Custom):
     def __init__(self, name, *args, **kwargs):
         super().__init__(name, "./images/crystal.png", *args, **kwargs)
 
+
+class Remix(Custom):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, "./images/remix.png", *args, **kwargs)
+
+
+class Tailscale(Custom):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, "./images/tailscale.png", *args, **kwargs)
+
+
+class Desktop(Custom):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, "./images/desktop.png", *args, **kwargs)
+
+
+class Laptop(Custom):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, "./images/laptop.png", *args, **kwargs)
+
+
+class CellPhone(Custom):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, "./images/cellphone.png", *args, **kwargs)
+
+
 with Diagram("infra", show=False, direction="TB"):
+    tailscale = Tailscale("Tailscale")
+
     with Cluster("DigitalOcean"):
         with Cluster("Personal"):
             macneil = Server("macneil")
@@ -27,6 +65,8 @@ with Diagram("infra", show=False, direction="TB"):
 
             with Cluster("cookie"):
                 cookie = Server("cookie")
+                tailscale << cookie
+
                 cookie_docker = Docker("docker")
                 cookie << cookie_docker
 
@@ -37,17 +77,17 @@ with Diagram("infra", show=False, direction="TB"):
                     jackharrhy << WebSite("jackharrhy.dev")
                     cookie_docker << jackharrhy
 
-                pad = Crystal("pad")
-                pad << WebSite("pad.jackharrhy.dev")
-                cookie_docker << pad
+                strickertrade = Remix("stickertrade")
+                strickertrade << WebSite("stickertrade.ca")
+                cookie_docker << strickertrade
 
                 livebook = Elixir("livebook")
                 livebook << WebSite("livebook.jackharrhy.dev")
                 cookie_docker << livebook
 
-                cbcrss = Crystal("cbcrss")
-                cbcrss << WebSite("cbc-rss.jackharrhy.dev")
-                cookie_docker << cbcrss
+                bar = Python("bar")
+                bar << WebSite("jackharrhy.dev/bar")
+                cookie_docker << bar
 
                 barab = JavaScript("barab")
                 barab << WebSite("jackharrhy.dev/barab")
@@ -57,17 +97,12 @@ with Diagram("infra", show=False, direction="TB"):
                 duaas << WebSite("jackharrhy.dev/random")
                 cookie_docker << duaas
 
-                bar = Python("bar")
-                bar << WebSite("jackharrhy.dev/bar")
-                cookie_docker << bar
-
-                metrobus_spy = Python("metrobus-spy")
-                metrobus_spy << WebSite("jackharrhy.dev/metrobus-spy")
-                cookie_docker << metrobus_spy
-
                 stackcoin = Crystal("stackcoin")
                 stackcoin << WebSite("stackcoin.world")
                 cookie_docker << stackcoin
+
+                phapbot = Php("Phapbot")
+                cookie_docker << phapbot
 
                 with Cluster("miniflux"):
                     miniflux = Go("miniflux")
@@ -78,11 +113,11 @@ with Diagram("infra", show=False, direction="TB"):
                     miniflux << miniflux_db
                     cookie_docker << miniflux_db
 
-                phapbot = Php("Phapbot")
-                cookie_docker << phapbot
+                traefik = Go("Traefik")
+                cookie_docker << traefik
 
-        with Cluster("saturn"):
-            Server("saturn")
+                watchtower = Go("Watchtower")
+                cookie_docker << watchtower
 
         with Cluster("MUNCS"):
             murray = Server("murray")
@@ -90,7 +125,17 @@ with Diagram("infra", show=False, direction="TB"):
             murray << Python("DiscordAuth")
 
     with Cluster("Home"):
-        system76 = Server("system76")
-        system76 << Java("cheesetown")
-
         stash = Storage("stash")
+        tailscale << stash
+
+        cardiff = Laptop("cardiff")
+        tailscale << cardiff
+
+        windows = Desktop("windows")
+        tailscale << windows
+
+    lemur = Laptop("lemur")
+    tailscale << lemur
+
+    pixel = CellPhone("pixel")
+    tailscale << pixel
